7a9fe87cc8c0610b8a5b627c9bbb576c9716c2b3
rian3005 <riannazar03@gmail.com>
2023-07-25 15:20:52 +0700

    add prisma

--- components/sections/blog/index.tsx
+++ components/sections/blog/index.tsx
@@ -4,9 +4,8 @@ import ListPost from "./listpost";
 export default function BlogPage() {
     return (
         <>
-        <TitlePage title="Blog" cari={true} desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda hic consequatur dolorem! Cumque beatae porro a natus quam amet autem!"/>
-        <ListPost/>
-        
+            <TitlePage title="Blog" cari={true} desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda hic consequatur dolorem! Cumque beatae porro a natus quam amet autem!" />
+            <ListPost />
         </>
     )
 }
