2b98d50afec680785f523a235c91342f456425c9
rian-nzr <riannazar03@gmail.com>
2023-08-10 00:11:37 +0700

    delete

--- components/sections/portfolio/listP.tsx
+++ components/sections/portfolio/listP.tsx
@@ -1,9 +1,3 @@
-import BlogList from "@/components/assets/bloglist";
-import CardBlog from "@/components/assets/cardblog";
-import BlogCardModeII from "@/components/assets/blogcardmodeii";
-import { useState } from "react";
-import Pagination from "@/components/assets/pagination";
-import { BsGrid, BsHddStack } from 'react-icons/bs'
 import CardP from "@/components/assets/cardP";
 
 export default function ListP() {
